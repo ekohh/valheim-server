116971e9e2f21a1dcb65600bc9f0fda67f0a863a
Brady Hoffart <brady2321@gmail.com>
2022-04-19 12:55:52 -0500

    created backup script, modified some log file locations, modified some plan

--- vhbackup-cron.sh
+++ vhbackup-cron.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Run valheim world backup nightly
-5 0 * * * /bin/bash /root/bin/vhbackup.sh >> /root/vhbackup.log 2>&1
+5 0 * * * /bin/bash /root/bin/vhbackup.sh >> /var/log/vhbackup.log 2>&1
---
+++ vhupdate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Options
+LOGFILE="/var/log/valheim-update.log"
+STEAMCMD="/home/steam/steamcmd.sh"
+VALHEIM_INSTALL_DIR="/home/steam/.steam/steamapps/common/valheim"
+
+# Update
+START="$(date +'%m-%d-%Y %H:%M:%S')"
+echo "${START} UPDATE: Starting update" >> $LOGFILE 2>&1
+su - steam -c "${STEAMCMD} +login anonymous +force_install_dir ${VALHEIM_INSTALL_DIR} +app_update 896660 validate +exit" >> $LOGFILE 2>&1 \
+    && echo "UPDATE: Update completed successfully" >> $LOGFILE 2>&1 \
+    || echo "UPDATE: Update failed" >> $LOGFILE 2>&1
+
+RESTART="$(date +'%m-%d-%Y %H:%M:%S')"
+echo "${RESTART} RESTART: Restarting Valheim service" >> $LOGFILE 2>&1
+systemctl restart valheim >> $LOGFILE 2>&1 \
+    && echo "RESTART: Restart completed successfully" >> $LOGFILE 2>&1 \
+    || echo "RESTART: Restart failed" >> $LOGFILE 2>&1
