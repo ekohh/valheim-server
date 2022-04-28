c7e31b5579d8a53b57e95d29945bf2ca91727d24
Brady Hoffart <brady2321@gmail.com>
2022-04-28 18:20:58 -0500

    fixed

--- tools/vhconn.sh
+++ tools/vhconn.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 CONN_LOG="/var/log/steam/valheim/valheim_log.txt"
-CURR_CONN="$(grep Connections /var/log/steam/valheim/valheim_log.txt | tail -n1 | cut -d' ' -f5')"
+CURR_CONN="$(grep Connections /var/log/steam/valheim/valheim_log.txt | tail -n1 | cut -d' ' -f5)"
 
 echo "Current connections: ${CURR_CONN}"
 
 if [ $CURR_CONN != "0" ]
 then
     COUNT=0
-    for VIKING in \
-    $(grep "ZDOID" /var/log/steam/valheim/valheim_log.txt \
-    | cut -d' ' -f7 | tail -n ${CURR_CONN}); \
-    do ONLINE[${COUNT}]=${VIKING} ; VIKINGS=$((${COUNT} + 1)); \
-    done
+    for VIKING in $(grep "ZDOID" /var/log/steam/valheim/valheim_log.txt | cut -d' ' -f7 | tail -n ${CURR_CONN})
+        do ONLINE[${COUNT}]=${VIKING} ; VIKINGS=$((${COUNT} + 1)) ; done
+    echo "Logged In: ${ONLINE[*]}"
 fi
